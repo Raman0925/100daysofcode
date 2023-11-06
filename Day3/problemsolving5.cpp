7a062019944415f76d4c6fa8ba49cb923b060bea
Raman <ramankumarkannaujiya@gmail.com>
2023-11-06 21:21:31 +0530

    Character Array

---
+++ Day3/problemsolving5.cpp
@@ -0,0 +1,49 @@
+//Length Of String
+#include<iostream>
+#include <cstring>
+using namespace std;
+//For Calculating length;
+int getLength(char name[]){
+     int length = 0;
+     int i =0;
+     while(name[i]!='\0'){
+        length++;
+        i++;
+
+     }
+     return length;
+}
+
+void replaceSpaces(char ch[]){
+    int length = strlen(ch);
+    for(int i=0;i<length;i++){
+        if(ch[i]==' '){
+            ch[i]='@';
+        }
+           
+    }
+}
+
+void reverseChar(char str[]) {
+    int length = strlen(str);
+    for (int i = 0; i < length / 2; i++) {
+        char temp = str[i];
+        str[i] = str[length - 1 - i];
+        str[length - 1 - i] = temp;
+    }
+}
+
+int main() {
+    char myString[] = "Hello, World!";
+    cout << "Original String: " << myString <<endl;
+    int length = getLength(myString);
+    cout<<"Length Of the String "<<length<<endl;
+
+    reverseChar(myString);
+    replaceSpaces(myString);
+
+    cout << "Reversed String and replace spaces " << myString  << endl;
+
+    return 0;
+}
+
